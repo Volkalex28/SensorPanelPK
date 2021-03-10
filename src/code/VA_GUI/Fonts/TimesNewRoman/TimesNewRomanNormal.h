985c751d58e061d8c2d0e640d6f23432161b430f
Volkalex28 <anan20000228@gmail.com>
2021-03-10 14:33:45 +0200

    Первый коммит

--- src/code/VA_GUI/Fonts/TimesNewRoman/TimesNewRomanNormal.h
+++
@@ -1,8 +0,0 @@
-#ifndef TimesNewRomanNormalH
-#define TimesNewRomanNormalH
-
-#include "TimesNewRoman.h"
-/*======= Character pointers table =======*/
-extern TCLISTP TimesNewRomanOrig[256];
-
-#endif
